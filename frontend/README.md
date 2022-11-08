9da6f9e95273475b8a68daa88ce24debbe001b45
F_Riazi <f_riazi@isc.co.ir>
2022-11-08 11:47:11 +0330

    first commit

---
+++ frontend/README.md
@@ -0,0 +1,5 @@
+it is a sample Project :booking Hotel 
+ book Room - admin panel - pay - add new room-
+
+i use 
+React Hook- Tailwind 
---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function App() {
+  return (
+    <div className='container flex justify-center items-center flex-col m-auto'>
+   <h1>RayRooms</h1>
+    </div>
+  )
+}
+
+export default App
